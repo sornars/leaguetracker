6e37580017ea055e242d1f915fc2117c8da634a6
Shivan Sornarajah <shivan@sornars.com>
2017-08-20 19:56:08 +0100

    Make LeagueType name unique

--- leagues/models.py
+++ leagues/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 
 class LeagueType(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class League(models.Model):
     name = models.CharField(max_length=50)
     league_type = models.ForeignKey(LeagueType, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name

174c85ef1ac92b81eec0e67c4d4699e3265c97ab
Shivan Sornarajah <shivan@sornars.com>
2017-08-20 19:31:33 +0100

    Added initial League and LeagueType models

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import League, LeagueType
+
+admin.site.register(League)
+admin.site.register(LeagueType)
--- leagues/models.py
+++ leagues/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class LeagueType(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class League(models.Model):
+    name = models.CharField(max_length=50)
+    league_type = models.ForeignKey(LeagueType, on_delete=models.PROTECT)

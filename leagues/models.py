207c2aa0766fc89973265e190957c58c5b613e17
Shivan Sornarajah <shivan@sornars.com>
2017-08-21 08:55:02 +0100

    Added Managers to League

--- leagues/models.py
+++ leagues/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -9,14 +10,15 @@ class LeagueType(models.Model):
         return self.name
 
 
+class Manager(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    team_name = models.CharField(max_length=50)
+
+
 class League(models.Model):
     name = models.CharField(max_length=50)
     league_type = models.ForeignKey(LeagueType, on_delete=models.PROTECT)
+    managers = models.ManyToManyField(Manager)
 
     def __str__(self):
         return self.name
-
-
-class Manager(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    team_name = models.CharField(max_length=50)

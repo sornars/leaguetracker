3004fe4c3bd94558da21d2ef856624740561a39d
Shivan Sornarajah <shivan@sornars.com>
2017-08-24 09:26:00 +0100

    Renamed class to FPLConfig

--- fpl/apps.py
+++ fpl/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class FplConfig(AppConfig):
+class FPLConfig(AppConfig):
     name = 'fpl'
--- leaguetracker/settings/base.py
+++ leaguetracker/settings/base.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'leagues.apps.LeaguesConfig',
+    'fpl.apps.FPLConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

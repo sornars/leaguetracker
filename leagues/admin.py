46fee8fbd982fee3afd2d082b3856b9efb8557c0
Shivan Sornarajah <shivan@sornars.com>
2017-09-20 19:35:05 +0100

    Removed Manager and Performance classes

--- leagues/admin.py
+++ leagues/admin.py
@@ -2,26 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import League, LeagueEntrant, LeagueType, Manager, Performance
+from .models import League, LeagueEntrant, LeagueType
 
 admin.site.register(League)
 admin.site.register(LeagueType)
-
-
-class ManagerInline(admin.StackedInline):
-    model = Manager
-    can_delete = False
-    verbose_name_plural = 'manager'
-    min_num = 1
-    extra = 0
-
-
-class UserAdmin(BaseUserAdmin):
-    inlines = (ManagerInline, )
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-
-admin.site.register(Manager)
-admin.site.register(Performance)
 admin.site.register(LeagueEntrant)
--- leagues/models.py
+++ leagues/models.py
@@ -5,47 +5,27 @@ from django.db import models
 
 class LeagueType(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    provider = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return '{provider} - {name}'.format(provider=self.provider, name=self.name)
 
-
-class Manager(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
-    team_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.team_name
+    class Meta:
+        unique_together = ('name', 'provider')
 
 
 class League(models.Model):
     name = models.CharField(max_length=50)
     league_type = models.ForeignKey(LeagueType, on_delete=models.CASCADE)
-    managers = models.ManyToManyField(Manager, blank=True, through='LeagueEntrant')
+    managers = models.ManyToManyField(User, blank=True, through='LeagueEntrant')
     entry_fee = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name
 
 
-class Performance(models.Model):
-    gameweek = models.IntegerField()
-    league_entrant = models.ForeignKey('LeagueEntrant', on_delete=models.CASCADE)
-    score = models.IntegerField()
-
-    def __str__(self):
-        return '{league_entrant} - {gameweek}: {score}'.format(
-            league_entrant=self.league_entrant,
-            gameweek=self.gameweek,
-            score=self.score
-        )
-
-    class Meta:
-        unique_together = ('gameweek', 'league_entrant')
-
-
 class LeagueEntrant(models.Model):
-    manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
     league = models.ForeignKey(League, on_delete=models.CASCADE)
     paid_entry = models.BooleanField()
 

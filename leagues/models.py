ab31a982443a9791544bc5eacc407ac8d4d74aa8
Shivan Sornarajah <shivan@sornars.com>
2017-09-21 11:07:10 +0100

    Added Payout, PositionPayout and PeriodPayout models

--- leagues/models.py
+++ leagues/models.py
@@ -33,3 +33,35 @@ class LeagueEntrant(models.Model):
 
     class Meta:
         unique_together = ('manager', 'league')
+
+
+class Payout(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    paid_out = models.BooleanField()
+    paid_to = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{league} - {name}: {amount}'.format(
+            league=self.league,
+            name=self.name,
+            amount=self.amount
+        )
+
+    class Meta:
+        abstract = True
+
+class PeriodPayout(Payout):
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    class Meta:
+        unique_together = ('league', 'name', 'start_date', 'end_date')
+
+
+class PositionPayout(Payout):
+    position = models.IntegerField()
+
+    class Meta:
+        unique_together = ('league', 'name', 'position')
